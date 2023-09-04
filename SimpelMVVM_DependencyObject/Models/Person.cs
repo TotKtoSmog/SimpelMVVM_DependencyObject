cf6f935da5aae53e33915753a7e3aaf319009124
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-09-05 00:31:36 +0300

    Добавьте файлы проекта.

---
+++ SimpelMVVM_DependencyObject/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpelMVVM_DependencyObject.Models
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public static Person[] GetPerrsons() 
+        {
+            Person[] result = new Person[]
+            {
+                new Person(){LastName ="Супрунов",FirstName = "Трофим", Phone="+7 (943) 700-72-15", Email="trofim61@rambler.ru"},
+                new Person(){LastName ="Заболотный",FirstName = "Илья", Phone="+7 (998) 722-50-62", Email="ilya47@outlook.com"},
+                new Person(){LastName ="Курушина",FirstName = "Лана", Phone="+7 (980) 522-98-79", Email="lana14021964@mail.ru"},
+                new Person(){LastName ="Мамонтова",FirstName = "Алина", Phone="+7 (947) 917-47-28", Email="alina1966@outlook.com"},
+                new Person(){LastName ="Лягушов",FirstName = "Трофим", Phone="+7 (931) 671-99-13", Email="trofim9911@ya.ru"},
+                new Person(){LastName ="Поджио",FirstName = "Даниил", Phone="+7 (906) 830-70-71", Email="daniil.podjio@rambler.ru"},
+                new Person(){LastName ="Заседателев",FirstName = "Никита", Phone="+7 (953) 185-49-71", Email="nikita.zasedatelev@rambler.ru"},
+                new Person(){LastName ="Ерофеева",FirstName = "Татьяна", Phone="+7 (992) 166-95-46", Email="tatyana7433@outlook.com"},
+                new Person(){LastName ="Осинова",FirstName = "Вероника", Phone="+7 (995) 462-35-49", Email="veronika70@gmail.com"},
+                new Person(){LastName ="Бендлина",FirstName = "Анна", Phone="+7 (944) 374-44-93", Email="anna26101984@hotmail.com"}
+            };
+            return result;
+        }
+    }
+}
---
+++ SimpelMVVM_DependencyObject/ViewModels/PersonsViewModel.cs
@@ -0,0 +1,65 @@
+﻿using SimpelMVVM_DependencyObject.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SimpelMVVM_DependencyObject.ViewModels
+{
+    internal class PersonsViewModel : DependencyObject
+    {
+
+
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilteerText", typeof(string), typeof(PersonsViewModel), new PropertyMetadata("", FilterText_Changed));
+
+        private static void FilterText_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var current = d as PersonsViewModel;
+            if (current != null)
+            {
+                current.Items.Filter = null;
+                current.Items.Filter = current.FilterPerson;
+            }
+        }
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(PersonsViewModel), new PropertyMetadata(null));
+
+        public PersonsViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Person.GetPerrsons());
+            Items.Filter = FilterPerson;
+        }
+
+        private bool FilterPerson(object obj)
+        {
+            bool result = true;
+            string _FilterText = FilterText.ToLower();
+            Person current = obj as Person;
+            if (!string.IsNullOrWhiteSpace(_FilterText) && current != null 
+                && !current.FirstName.ToLower().Contains(_FilterText)
+                && !current.LastName.ToLower().Contains(_FilterText))
+                result = false;
+            return result;
+        }
+    }
+}
